a6497edc84df257399cb2c9922d68fd84e5b4fa5
GeofferyThornock <geofferythornock@gmail.com>
2023-07-06 10:29:50 -0500

    fixing production url not updated

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,15 @@
 // Update with your config settings.
 require("dotenv").config();
 const path = require("path");
-const { DATABASE_URL } = process.env;
+const {
+    NODE_ENV = "development",
+    DEVELOPMENT_DATABASE_URL,
+    PRODUCTION_DATABASE_URL,
+} = process.env;
+const URL =
+    NODE_ENV === "production"
+        ? PRODUCTION_DATABASE_URL
+        : DEVELOPMENT_DATABASE_URL;
 
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
@@ -9,7 +17,17 @@ const { DATABASE_URL } = process.env;
 module.exports = {
     development: {
         client: "postgresql",
-        connection: DATABASE_URL,
+        connection: URL,
+        migrations: {
+            directory: path.join(__dirname, "src", "db", "migrations"),
+        },
+        seeds: {
+            directory: path.join(__dirname, "src", "db", "seeds"),
+        },
+    },
+    production: {
+        client: "postgresql",
+        connection: URL,
         migrations: {
             directory: path.join(__dirname, "src", "db", "migrations"),
         },
